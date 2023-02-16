172682beb2d683434939616dedf7598511afcc52
Mariam-Adly <mariamadly712@gmail.com>
2023-02-16 16:02:26 +0200

    add landscape screen

--- app/src/main/java/com/example/mealplanner/network/RetrofitManager.java
+++ app/src/main/java/com/example/mealplanner/network/RetrofitManager.java
@@ -3,7 +3,7 @@ package com.example.mealplanner.network;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
